0db09a059e03f4879d545b73884d634cd3352f48
mafmeera <meera@unitdtechnologies.com>
2022-11-03 12:17:52 +0530

    tender.js

--- routes/tender.js
+++ routes/tender.js
@@ -612,6 +612,56 @@ app.post('/insertCostingSummary', (req, res, next) => {
   });
 });
 
+app.post('/insertOpportunityCostingSummary', (req, res, next) => {
+
+  let data = {supplier_id: req.body.supplier_id
+    , sub_con_id: req.body.sub_con_id
+    , product_id: req.body.product_id
+    , opportunity_id: req.body.opportunity_id
+    , sketch: req.body.sketch
+    , quantity: req.body.quantity
+    , unit_price: req.body.unit_price
+    , amount: req.body.amount
+    , creation_date: req.body.creation_date
+    , modification_date: req.body.modification_date
+    , opportunity_costing_summary_id: req.body.opportunity_costing_summary_id
+    , unit: req.body.unit
+    
+    };
+  let sql = "INSERT INTO opportunity_costing_summary SET ?";
+  let query = db.query(sql, data,(err, result) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    } else {
+          return res.status(200).send({
+            data: result,
+            msg:'New Tender has been created successfully'
+          });
+    }
+  });
+});
+
+
+app.delete('/deleteTender', (req, res, next) => {
+
+  let data = {opportunity_id: req.body.opportunity_id};
+  let sql = "DELETE FROM opportunity WHERE ?";
+  let query = db.query(sql, data,(err, result) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    } else {
+          return res.status(200).send({
+            data: result,
+            msg:'Tender has been removed successfully'
+          });
+    }
+  });
+});
+
 
 app.get('/secret-route', userMiddleware.isLoggedIn, (req, res, next) => {
   console.log(req.userData);
