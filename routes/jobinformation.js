9de97b144e6f208132e7a2ebf9a4746b1b242d96
meera <meeera520@gmail.com>
2022-11-16 11:28:08 +0530

    joninformation-insert query

--- routes/jobinformation.js
+++ routes/jobinformation.js
@@ -170,7 +170,95 @@ app.post('/editjobinformation', (req, res, next) => {
                   }
                 });
               });
+    
+    
+              app.post('/insertjob_information', (req, res, next) => {
 
+                let data = {designation: req.body.designation,
+                  probationary: req.body.probationary,
+                  emp_type: req.body.emp_type,
+                  join_date: req.body.join_date,
+                  act_join_date: req.body.act_join_date,
+                  termination_date: req.body.termination_date,
+                  termination_reason: req.body.termination_reason,
+                  department: req.body.department,
+                  basic_pay: req.body.basic_pay,
+                  levy_amount: req.body.levy_amount,
+                  working_days: req.body.working_days,
+                  payment_type: req.body.payment_type,
+                  overtime: req.body.overtime,
+                  overtime_pay_rate: req.body.overtime_pay_rate,
+                  cpf_applicable: req.body.cpf_applicable,
+                  income_tax_id: req.body.income_tax_id,
+                  pay_cdac: req.body.pay_cdac,
+                  pay_sinda: req.body.pay_sinda,
+                  pay_mbmf: req.body.pay_mbmf,
+                  cpf_account_no: req.body.cpf_account_no,
+                  pay_eucf: req.body.pay_eucf,
+                  mode_of_payment: req.body.mode_of_payment,
+                  account_no: req.body.account_no,
+                  bank_name: req.body.bank_name,
+                  bank_code: req.body.bank_code,
+                  branch_code: req.body.branch_code,
+                  creation_date: req.body.creation_date,
+                  modification_date: req.body.modification_date,
+                  created_by: req.body.created_by,
+                  modified_by: req.body.modified_by,
+                  flag: req.body.flag,
+                  allowance1: req.body.allowance1,
+                  status: req.body.status,
+                  income_tax_amount: req.body.income_tax_amount,
+                  allowance2: req.body.allowance2,
+                  allowance3: req.body.allowance3,
+                  deduction1: req.body.deduction1,
+                  deduction2: req.body.deduction2,
+                  deduction3: req.body.deduction3,
+                  deduction4: req.body.deduction4,
+                  employee_id: req.body.employee_id,
+                  govt_donation: req.body.govt_donation,
+                  departure_date: req.body.departure_date,
+                  resignation_notice_date: req.body.resignation_notice_date,
+                  work_hour_details: req.body.work_hour_details,
+                  duty_responsibility: req.body.duty_responsibility,
+                  rest_day_per_week: req.body.rest_day_per_week,
+                  probation_start_date: req.body.probation_start_date,
+                  probation_end_date: req.body.probation_end_date,
+                  length_of_probation: req.body.length_of_probation,
+                  notice_period_for_termination: req.body.notice_period_for_termination,
+                  duration_of_employment: req.body.duration_of_employment,
+                  place_of_work: req.body.place_of_work,
+                  salary_payment_dates : req.body. salary_payment_dates ,
+                  overtime_payment_dates: req.body.overtime_payment_dates,
+                  paid_annual_leave_per_year: req.body.paid_annual_leave_per_year,
+                  paid_outpatient_sick_leave_per_year: req.body.paid_outpatient_sick_leave_per_year,
+                  paid_hospitalisation_leave_per_year: req.body.paid_hospitalisation_leave_per_year,
+                  other_type_of_leave: req.body.other_type_of_leave,
+                  paid_medical_examination_fee: req.body.paid_medical_examination_fee,
+                  other_medical_benefits: req.body.other_medical_benefits,
+                  allowance4: req.body.allowance4,
+                  allowance5: req.body.allowance5,
+                  allowance6: req.body.allowance6,
+                  site_id: req.body.site_id,
+                  over_time_rate: req.body.over_time_rate,
+                  
+                };
+                  
+              
+                let sql = "INSERT INTO job_information SET ?";
+                let query = db.query(sql, data,(err, result) => {
+                  if (err) {
+                    console.log("error: ", err);
+                    result(err, null);
+                    return;
+                  } else {
+                        return res.status(200).send({
+                          data: result,
+                          msg:'New Tender has been created successfully'
+                        });
+                  }
+                });
+              });
+              
               
 app.get('/secret-route', userMiddleware.isLoggedIn, (req, res, next) => {
     console.log(req.userData);
